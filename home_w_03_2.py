94d41d3d7317d31ba85fe0c06bbf31fd3beb4826
nrdwrr <159884235+nrdwrr@users.noreply.github.com>
2024-03-12 11:17:02 +0200

    Add files via upload

---
+++ home_w_03_2.py
@@ -0,0 +1,17 @@
+# Друге завдання
+
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if min >= 1 and max <= 1000 and quantity >= min and quantity <= max:
+        numbers = random.sample(range(min, max + 1), quantity)
+        return sorted(numbers)
+    
+    else:
+        numbers = []
+        return numbers
+
+
+lottery_numbers = get_numbers_ticket(1, 49, 6)
+print("Ваші лотерейні числа:", lottery_numbers)
+
