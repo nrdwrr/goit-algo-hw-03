2c65dbebf8aeb14b23a1da575fd1525df6c12220
nrdwrr <159884235+nrdwrr@users.noreply.github.com>
2024-04-26 10:25:01 +0300

    Add files via upload

--- home_w_03_1.py
+++ home_w_03_1.py
@@ -3,18 +3,24 @@
 
 from datetime import datetime
 
+def get_days_from_today(date):
+        
+    try:
+
+        date_formated = datetime.strptime(date, "%Y-%m-%d")
+        current_date = datetime.today()
+        get_days_from_today = current_date.toordinal() - date_formated.toordinal()
+        return int(get_days_from_today)
+
+    except ValueError:
+
+        return(f"некоректні вхідні дані: {date}. спробуйте ще")
+
+       
+
 date = input("Введіть дату у форматі 'РРРР-ММ-ДД': ")
 
-def get_days_from_today(date):
-    date_formated = datetime.strptime(date, "%Y-%m-%d")
-    current_date = datetime.today()
-    get_days_from_today = current_date.toordinal() - date_formated.toordinal()
-    return int(get_days_from_today)
 
-try:
-     print(f"кількість днів між заданою датою {date} і поточною датою  складає {get_days_from_today(date)}")
+print(f"кількість днів між заданою датою {date} і поточною датою  складає: {get_days_from_today(date)}")
 
-except ValueError:
-    print(f"некоректні вхідні дані {date} спробуйте ще")
-    
 
